619f3525ead6c0a56d95c86bf4a358f89b503553
buguenocesar92 <buguenocesar92@gmail.com>
2024-12-27 22:03:37 -0300

    Initialize Laravel project structure with essential files and configurations

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::group([
+    //'middleware' => 'api',
+    'prefix' => 'auth',
+    //'middleware' => ['auth:api', 'role:Super-Admin'],
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout'); //->middleware('auth:api')
+    Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/me', [AuthController::class, 'me'])->name('me');
+});
