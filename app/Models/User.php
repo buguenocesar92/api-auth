0afb1d843ab0ed12789bf73f7c0976b42f83acfc
buguenocesar92 <buguenocesar92@gmail.com>
2024-12-30 16:39:01 -0300

    Refactor User model to include HasRoles trait and clean up unused code

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,13 +10,10 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
-    use HasRoles;
+    use HasFactory, Notifiable, HasRoles;
 
-    protected $guard_name = 'api';
     protected $fillable = ['name', 'email', 'password', 'tenant_id'];
 
-    // Métodos requeridos por JWTSubject
     public function getJWTIdentifier()
     {
         return $this->getKey();
