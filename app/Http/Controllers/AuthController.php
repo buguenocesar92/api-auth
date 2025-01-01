361accbfac69e02a718a9c44d5935abee7071904
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-01 15:07:31 -0300

    Fix import statement for Validator in AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Validator;
+use Illuminate\Support\Facades\Validator;
 
 
 class AuthController extends Controller
