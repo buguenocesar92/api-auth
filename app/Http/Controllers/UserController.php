153c0fb134ec2c381e3567b1c96b36c2eecd232b
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-01 15:07:39 -0300

    Add listUsersByTenant method to retrieve users by tenant

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function assignAdmin(Request $request)
+/*     public function assignAdmin(Request $request)
     {
         // Validar que venga un user_id o user_email
         $request->validate([
@@ -23,6 +24,17 @@ class UserController extends Controller
         return response()->json([
             'message' => "User {$user->name} has been assigned the 'admin' role.",
             'user' => $user
+        ]);
+    } */
+    public function listUsersByTenant()
+    {
+        // Obtener usuarios del tenant
+        $users = User::where('tenant_id', Auth::user()->tenant_id)->get();
+
+        // Retornar los usuarios como respuesta JSON
+        return response()->json([
+            'message' => 'Usuarios obtenidos exitosamente.',
+            'users' => $users
         ]);
     }
 
